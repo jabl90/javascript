27d54bfc1b6decef18ef61b924ebb3453e36a433
jabl90 <jose.barealpz@gmail.com>
2018-10-27 23:47:14 +0200

    javascript
    javascript pagina geta

---
+++ dom/pagina.js
@@ -0,0 +1,14 @@
+
+document.getElementById('M').onclick=function(){
+
+  document.getElementById('par').innerHTML=document.getElementById('par').innerHTML.toUpperCase();
+
+}
+
+
+document.getElementById('minusculas').onclick=function(){
+
+  document.getElementById('var').innerHTML=document.getElementById('var').innerHTML.toLowerCase();
+
+}
+
